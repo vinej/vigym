37c3c08578731b651f0df61ea66002ae672100ef
jean-yves.vinet <jyvinet@hotmail.ca>
2018-04-05 11:04:56 -0400

    docs

--- preact.config.js
+++ preact.config.js
@@ -2,8 +2,10 @@
 
 /*https://dzone.com/articles/build-a-preact-app-with-authentication*/
 /* https://www.codementor.io/drewpowers/high-performance-webpack-config-for-front-end-delivery-90sqic1qa */
+
 export default (config, env, helpers) => {
 	let { plugin } = helpers.getPluginsByName(config, "UglifyJsPlugin")[0];
 	plugin.options.sourceMap = false
 }
+
 
