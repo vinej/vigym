fbce6cf9b50a321752cebc0537c927a822aee4c1
jean-yves.vinet <jyvinet@hotmail.ca>
2018-04-05 10:41:38 -0400

    no maps

--- preact.config.js
+++ preact.config.js
@@ -1,29 +1,9 @@
-const preactCliSwPrecachePlugin = require('preact-cli-sw-precache');
+//const preactCliSwPrecachePlugin = require('preact-cli-sw-precache');
 
 /*https://dzone.com/articles/build-a-preact-app-with-authentication*/
 /* https://www.codementor.io/drewpowers/high-performance-webpack-config-for-front-end-delivery-90sqic1qa */
-
-export default function (config) {
-	const precacheConfig = {
-		staticFileGlobs: [
-			'build/**/*.{html,js,css,jpg}',
-			'build/assets/favicon.ico',
-			'build/assets/icons/*.png'
-		],
-		stripPrefix: 'build/',
-		navigateFallback: '/index.html',
-		runtimeCaching: [{
-			urlPattern: /\/api\//,
-			handler: 'networkOnly'
-		}, {
-			urlPattern: '.*',
-			handler: 'cacheFirst'
-		}],
-		minify: true,
-		clientsClaim: true,
-		skipWaiting: true,
-		maximumFileSizeToCacheInBytes: 10*1024*1024
-	};
-
-	return preactCliSwPrecachePlugin(config, precacheConfig);
+export default (config, env, helpers) => {
+	let { plugin } = helpers.getPluginsByName(config, "UglifyJsPlugin")[0];
+	plugin.options.sourceMap = false
 }
+
