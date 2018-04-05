d6813d34bf1bf89517309cc2c12dd79f2104fb29
jean-yves.vinet <jyvinet@hotmail.ca>
2018-04-05 12:34:00 -0400

    docs

--- preact.config.js
+++ preact.config.js
@@ -11,27 +11,27 @@ const preactCliSwPrecachePlugin = require('preact-cli-sw-precache');
 		let { plugin } = helpers.getPluginsByName(config, "UglifyJsPlugin")[0];
 		plugin.options.sourceMap = false
 
-		const precacheConfig = { 
-			staticFileGlobs: [ 
-				'build/**/*.{html,js,css,jpg}', 
-				'build/assets/favicon.ico', 
-				'build/assets/icons/*.png' 
-			], 
-			stripPrefix: 'build/', 
-			navigateFallback: '/index.html', 
-			runtimeCaching: [{ 
-				urlPattern: /\/api\//, 
-				handler: 'networkOnly' 
-			}, { 
-				urlPattern: '.*', 
-				handler: 'cacheFirst' 
-			}], 
-			minify: true, 
-			clientsClaim: true, 
-			skipWaiting: true, 
-			maximumFileSizeToCacheInBytes: 10*1024*1024 
-		}
-		return preactCliSwPrecachePlugin(config, precacheConfig); 
+		//const precacheConfig = { 
+		//	staticFileGlobs: [ 
+		//		'build/**/*.{html,js,css,jpg}', 
+		//		'build/assets/favicon.ico', 
+		//		'build/assets/icons/*.png' 
+		//	], 
+		//	stripPrefix: 'build/', 
+		//	navigateFallback: '/index.html', 
+		//	runtimeCaching: [{ 
+		//		urlPattern: /\/api\//, 
+		//		handler: 'networkOnly' 
+		//	}, { 
+		//		urlPattern: '.*', 
+		//		handler: 'cacheFirst' 
+		//	}], 
+		//	minify: true, 
+		//	clientsClaim: true, 
+		//	skipWaiting: true, 
+		//	maximumFileSizeToCacheInBytes: 10*1024*1024 
+		//}
+		//return preactCliSwPrecachePlugin(config, precacheConfig); 
 	} 	
 }; 
  
