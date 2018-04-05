dae06ec2504adbd51437cd320f6787f7ce4b87d3
jean-yves.vinet <jyvinet@hotmail.ca>
2018-04-05 11:44:40 -0400

    docs

--- preact.config.js
+++ preact.config.js
@@ -4,29 +4,34 @@ const preactCliSwPrecachePlugin = require('preact-cli-sw-precache');
  /* https://www.codementor.io/drewpowers/high-performance-webpack-config-for-front-end-delivery-90sqic1qa */ 
  export default function (config, env, helpers) { 
 
-	let { plugin } = helpers.getPluginsByName(config, "UglifyJsPlugin")[0];
-	plugin.options.sourceMap = false
+	//let { plugin } = helpers.getPluginsByName(config, "UglifyJsPlugin")[0];
+	//plugin.options.sourceMap = false
+	//console.log(config);
+	if (env.production) {
+		let { plugin } = helpers.getPluginsByName(config, "UglifyJsPlugin")[0];
+		plugin.options.sourceMap = false
 
- 	const precacheConfig = { 
- 		staticFileGlobs: [ 
- 			'docs/**/*.{html,js,css,jpg}', 
- 			'docs/assets/favicon.ico', 
- 			'docs/assets/icons/*.png' 
- 		], 
- 		stripPrefix: 'docs/', 
- 		navigateFallback: '/index.html', 
- 		runtimeCaching: [{ 
- 			urlPattern: /\/api\//, 
- 			handler: 'networkOnly' 
- 		}, { 
- 			urlPattern: '.*', 
- 			handler: 'cacheFirst' 
- 		}], 
- 		minify: true, 
- 		clientsClaim: true, 
- 		skipWaiting: true, 
- 		maximumFileSizeToCacheInBytes: 10*1024*1024 
- 	}; 
-  
- 	return preactCliSwPrecachePlugin(config, precacheConfig); 
-} 
+		const precacheConfig = { 
+			staticFileGlobs: [ 
+				'build/**/*.{html,js,css,jpg}', 
+				'build/assets/favicon.ico', 
+				'build/assets/icons/*.png' 
+			], 
+			stripPrefix: 'build/', 
+			navigateFallback: '/index.html', 
+			runtimeCaching: [{ 
+				urlPattern: /\/api\//, 
+				handler: 'networkOnly' 
+			}, { 
+				urlPattern: '.*', 
+				handler: 'cacheFirst' 
+			}], 
+			minify: true, 
+			clientsClaim: true, 
+			skipWaiting: true, 
+			maximumFileSizeToCacheInBytes: 10*1024*1024 
+		}
+		return preactCliSwPrecachePlugin(config, precacheConfig); 
+	} 	
+}; 
+ 
--- src/components/header/index.js
+++ src/components/header/index.js
@@ -10,7 +10,7 @@ export default class Header extends Component {
 	render() {
 		return (
 			<header class={style.header}>
-				<h1>ViGym</h1>
+				<h1>ViGym v1</h1>
 				<nav>
 					<Link activeClassName={style.active} href="/vigym">Home</Link>
 					<Link activeClassName={style.active} href="/vigym/profile">Me</Link>
